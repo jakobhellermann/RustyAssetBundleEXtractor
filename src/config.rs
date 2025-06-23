e10736e0b28d757cad60f01e587c099eb5f19ff3
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-06-23 09:09:40 +0200

    bundlefile: use typed UnityVersion

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,12 @@
+use crate::unity_version::{UnityVersion, UnityVersionType};
+
 pub struct ExtractionConfig {
     pub unitycn_key: Option<[u8; 16]>,
-    pub fallback_unity_version: String,
+    pub fallback_unity_version: UnityVersion,
 }
 
 impl ExtractionConfig {
-    pub fn new(unitycn_key: Option<[u8; 16]>, fallback_unity_version: String) -> Self {
+    pub fn new(unitycn_key: Option<[u8; 16]>, fallback_unity_version: UnityVersion) -> Self {
         Self {
             unitycn_key,
             fallback_unity_version,
@@ -21,7 +23,13 @@ impl Default for ExtractionConfig {
     fn default() -> Self {
         Self {
             unitycn_key: None,
-            fallback_unity_version: "2.5.0f5".to_owned(),
+            fallback_unity_version: UnityVersion {
+                major: 2,
+                minor: 5,
+                build: 0,
+                typ: UnityVersionType::Final,
+                build_number: 5,
+            },
         }
     }
 }
