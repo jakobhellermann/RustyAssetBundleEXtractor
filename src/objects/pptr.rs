8bf5a0065e497aaad096572e2d7f1c6384512c74
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-06-23 13:33:24 +0200

    remove old ObjectHandler

--- src/objects/pptr.rs
+++ src/objects/pptr.rs
@@ -70,20 +70,6 @@ impl PPtr {
         assert!(self.is_local(), "Non-local pptr in deref_read_local");
         file.get_object(self.m_PathID, tpk)
     }
-
-    pub fn get_object_handler<'a, R: std::io::Read + std::io::Seek>(
-        &'a self,
-        asset: &'a crate::files::SerializedFile,
-        reader: &'a mut R,
-    ) -> Result<crate::files::ObjectHandler<'a, R>, std::io::Error> {
-        match asset.get_object_info(self.m_PathID) {
-            Some(objectinfo) => Ok(asset.get_object_handler(objectinfo, reader)),
-            None => Err(std::io::Error::new(
-                std::io::ErrorKind::NotFound,
-                "Object not found",
-            )),
-        }
-    }
 }
 
 #[derive(Serialize, Deserialize)]
