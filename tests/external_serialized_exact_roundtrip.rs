522a4b919d3a7d12dafeb5a8943bfd3c566ea7bd
Jakob Hellermann <jakob.hellermann@protonmail.com>
2026-05-24 12:06:56 +0200

    tests: improve external_roundtrip_levels error

--- tests/external_serialized_exact_roundtrip.rs
+++ tests/external_serialized_exact_roundtrip.rs
@@ -3,11 +3,12 @@ mod external_test_data;
 use std::io::Cursor;
 use std::path::Path;
 
+use anyhow::{Context, Result, bail};
 use rabex::files::{SerializedFile, serializedfile};
 use rabex::tpk::TpkTypeTreeBlob;
 
 #[test]
-fn external_roundtrip_levels() -> Result<(), std::io::Error> {
+fn external_roundtrip_levels() -> Result<()> {
     let tt = TpkTypeTreeBlob::embedded();
 
     for game_path in external_test_data::EXTERNAL_GAME_PATHS {
@@ -18,13 +19,14 @@ fn external_roundtrip_levels() -> Result<(), std::io::Error> {
             if !filename.starts_with("level") {
                 continue;
             }
-            assert_roundtrip(&entry.path(), &tt)?;
+            assert_roundtrip(&entry.path(), &tt)
+                .with_context(|| format!("Could not roundtrip {}", entry.file_name().display()))?;
         }
     }
     Ok(())
 }
 
-fn assert_roundtrip(path: &Path, tt: &TpkTypeTreeBlob) -> Result<(), std::io::Error> {
+fn assert_roundtrip(path: &Path, tt: &TpkTypeTreeBlob) -> Result<()> {
     println!("{}", path.display());
     let data = std::fs::read(path)?;
 
@@ -39,7 +41,15 @@ fn assert_roundtrip(path: &Path, tt: &TpkTypeTreeBlob) -> Result<(), std::io::Er
 
     let start = 0;
     let up_to = out.get_ref().len().min(data.len());
-    pretty_assertions::assert_eq!(&data[start..up_to], &out.get_ref()[start..up_to]);
+
+    let left = &data[start..up_to];
+    let right = &out.get_ref()[start..up_to];
+
+    if left != right {
+        // let comparison = pretty_assertions::Comparison::new(left, right);
+        // bail!("Failed to roundtrip: {}", comparison);
+        bail!("error roundtripping {}", path.display());
+    }
 
     // unity aligns at begin, unitypy at end of loop
     assert_eq!(data.len(), out.get_ref().len());
